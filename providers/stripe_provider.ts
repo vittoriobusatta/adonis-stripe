6415b499be41e1dc7824552602a6b090209340a7
Vittorio <tonibusatta@gmail.com>
2024-12-06 22:08:40 +0100

    refactor(provider): move StripeService initialization from boot to register

--- providers/stripe_provider.ts
+++ providers/stripe_provider.ts
@@ -1,20 +1,16 @@
 import type { ApplicationService } from '@adonisjs/core/types'
 import type { StripeConfig } from '../src/types/main.js'
-
-import { StripeService } from '../src/stripe.js'
+import StripeService from '../src/stripe.js'
 
 export default class StripeProvider {
-  #stripe: StripeService | null = null
-
   constructor(protected app: ApplicationService) {}
 
-  async boot() {
-    const config = this.app.config.get<StripeConfig>('stripe')
-    const logger = await this.app.container.make('logger')
+  register() {
+    this.app.container.singleton('stripe', async () => {
+      const config = this.app.config.get<StripeConfig>('stripe')
+      const logger = await this.app.container.make('logger')
 
-    this.app.container.bind(StripeService, async () => {
-      this.#stripe = new StripeService(logger, config)
-      return this.#stripe
+      return new StripeService(logger, config)
     })
   }
 }
