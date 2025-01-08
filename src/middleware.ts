65ce711905b823e9cd684b541d289adb4036efd0
Vittorio <tonibusatta@gmail.com>
2025-01-08 14:00:03 +0100

    add missing inject decorator in InitializeStripeWebhookMiddleware (#1)

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,9 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 import StripeService from './stripe.js'
+import { inject } from '@adonisjs/core'
 
+@inject()
 export default class InitializeStripeWebhookMiddleware {
   constructor(protected stripe: StripeService) {}
 
